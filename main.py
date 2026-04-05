3893799c0500a22c248ddd9ae68af6c3127cbd7e
Larzous <larzous@gmail.com>
2026-04-05 18:16:49 -0400

    import dotenv and handle missing api key from .env file

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
 def main():
+    if api_key == None:
+        raise RuntimeError("Missing Gemini API Key")
     print("Hello from ai-agent!")
 
 
